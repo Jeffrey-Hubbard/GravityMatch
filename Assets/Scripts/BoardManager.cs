5ba2ab107404be5e54c3fef8650166c0e73e6c65
Jeffrey Hubbard <jeffrey.hubbard@gmail.com>
2018-07-19 19:03:45 -0700

    Update new object creation and orientation

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,62 +1,102 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class BoardManager : MonoBehaviour {
+public class BoardManager : MonoBehaviour
+{
 
     public static BoardManager boardManager;
     public List<Sprite> symbols = new List<Sprite>();
-    public GameObject tile;
-    public int xSize, ySize;
+    public Tile tile;
+    public static int xSize = 9;
+    public static int ySize = 9;
 
-    private GameObject[,] tiles;
+    public Tile[,] tiles;
 
     public bool IsMoving { get; set; }
 
+    public List<string> MatchColor = new List<string> { "blue", "green", "black", "orange", "brown", "grey"};
+    public List<string> ShipType = new List<string> { "fighter", "bomber", "transport", "asteroid", "bonus"};
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         boardManager = GetComponent<BoardManager>();
 
         Vector2 offset = tile.GetComponent<BoxCollider2D>().size;
 
         MakeBoard(offset.x, offset.y);
-	}
-	
+    }
+
     private void MakeBoard(float xOffset, float yOffset)
     {
-        tiles = new GameObject[xSize, ySize];
+        tiles = new Tile[xSize, ySize];
 
         float originX = transform.position.x;
         float originY = transform.position.y;
 
-        Sprite[] previousLeftList = new Sprite[ySize];
-        Sprite previousBelow = null;
+        string[] previousLeftList = new string[ySize];
+        string previousBelow = null;
 
         for (int x = 0; x < xSize; x++)
         {
             for (int y = 0; y < ySize; y++)
             {
-                GameObject newTile = Instantiate(tile, new Vector3(originX + (xOffset * x), originY + (yOffset * y), 0), tile.transform.rotation);
+                Tile newTile = Instantiate(tile, new Vector3(originX + (xOffset * x), originY + (yOffset * y), 0), tile.transform.rotation);
                 newTile.name = " Tile at " + x + " " + y;
 
                 tiles[x, y] = newTile;
                 newTile.transform.parent = transform;
 
-                List<Sprite> possibleSymbols = new List<Sprite>();
-                possibleSymbols.AddRange(symbols);
-                possibleSymbols.Remove(previousLeftList[y]);
-                possibleSymbols.Remove(previousBelow);
+                List<string> possibleColors = new List<string>();
+                possibleColors.AddRange(MatchColor);
+                possibleColors.Remove("bonus");
+                possibleColors.Remove("random");
+                possibleColors.Remove("none");
+                possibleColors.Remove(previousLeftList[y]);
+                possibleColors.Remove(previousBelow);
 
-                Sprite newSprite = possibleSymbols[Random.Range(0, possibleSymbols.Count)];
-                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
-                previousLeftList[y] = newSprite;
-                previousBelow = newSprite;
+                //Sprite newSprite = possibleSymbols[Random.Range(0, possibleSymbols.Count)];
+                string newColor = possibleColors[UnityEngine.Random.Range(0, possibleColors.Count)];
+                Debug.Log("Setting sprite at " + x + ", " + y + " to " + newColor);
+                newTile.Initialize(matchColor : newColor);
+
+
+                previousLeftList[y] = newColor;
+                previousBelow = newColor;
             }
         }
     }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void ClearMatches()
+    {
+        bool FoundAnyMatches = false;
+        boardManager.IsMoving = true;
+        Tile[,] tempTiles = tiles;
+        Tile[,] matchedTiles = new Tile[xSize, ySize];
+
+        //begin a loop through the tempTiles
+        foreach (Tile tile in tempTiles)
+        {
+            int howManyMatches = 0;
+            // is tile a valid matched tile? (reuse code from movement check)
+                
+            // if yes, then set FoundAnyMatches to true, then
+            // mark all matched tiles in matchedTiles and remove from temptiles
+
+            // based on value of howManyMatches either replace tile with a bonus tile at tile location...
+
+            // ... or remove tile from temptiles
+
+        }
+
+        // If the program FoundAnyMatches then
+        // Call function to fill in empty spaces
+
+        //else return control to player
+        
+
+    }
 }
+
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,20 +1,99 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour {
     private static Tile previousTile = null;
-    public Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
 
     private SpriteRenderer render;
     private bool isSelected = false;
 
     private Vector2[] adjacentCells = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
 
-    public enum matchColor { blue, green, black, orange, brown, grey}
+    //public enum MatchColor { blue, green, black, orange, brown, grey, none, random}
+    //public enum ShipType { fighter, bomber, transport, meteor, bonus, none, random}
 
-	// Use this for initialization
-	void Awake ()
+    public List<string> MatchColors = new List<string> { "blue", "green", "black", "orange", "brown", "grey" };
+    public List<string> ShipTypes = new List<string> { "fighter", "bomber", "transport", "asteroid" };
+
+    public string matchColor;
+    public string shipType;
+
+    // Use this for initialization
+
+    public void Initialize()
+    {
+         matchColor = GetRandomMatchColor();
+        // All grey and brown colored icons are asteroids
+        if (matchColor == "grey" || matchColor == "brown")
+        {
+            shipType = "asteroid";
+        }
+        // Black and Orange icons are enemies (pick randomly from ship types)
+        if (matchColor == "black" || matchColor == "orange")
+        {
+            shipType = ShipTypes[UnityEngine.Random.Range(0, ShipTypes.Count - 1)];
+        }
+        // Green and Blue icons are friendly ships (pick randomly from ship types)
+        if (matchColor == "blue" || matchColor == "green")
+        {
+            shipType = ShipTypes[UnityEngine.Random.Range(0, ShipTypes.Count - 1)];
+        }
+
+
+        string spriteName = shipType + "_" + matchColor;
+        render.sprite = Resources.Load<Sprite>(spriteName);
+        transform.Rotate(0.0f, 0.0f, UnityEngine.Random.Range(0, 3) * 90);
+
+    }
+
+    public void Initialize( string matchColor )
+    {
+        if (matchColor == "random")
+        {
+            matchColor = GetRandomMatchColor();
+        }
+
+        // All grey and brown colored icons are asteroids
+        if (matchColor == "grey" || matchColor == "brown")
+        {
+            shipType = "asteroid";
+        }
+        // Black and Orange icons are enemies (pick randomly from ship types)
+        if (matchColor == "black" || matchColor == "orange")
+        {
+            shipType = ShipTypes[UnityEngine.Random.Range(0, ShipTypes.Count - 1)];
+        }
+        // Green and Blue icons are friendly ships (pick randomly from ship types)
+        if (matchColor == "blue" || matchColor == "green")
+        {
+            shipType = ShipTypes[UnityEngine.Random.Range(0, ShipTypes.Count - 1)];
+        }
+
+
+        string spriteName = shipType + "_" + matchColor;
+        render.sprite = Resources.Load<Sprite>(spriteName);
+        transform.Rotate(0.0f, 0.0f, UnityEngine.Random.Range(0, 3) * 90);
+    }
+
+    //public void Initialize(string matchColor = "random", string shipType = "random")
+    //{
+    //    if (shipType == "random")
+    //    {
+    //        shipType = GetRandomShipType();
+    //    }
+    //    if (matchColor == "random")
+    //    {
+    //        matchColor = GetRandomMatchColor();
+    //    }
+
+
+    //    string spriteName = shipType + "_" + matchColor;
+    //    render.sprite = Resources.Load<Sprite>(spriteName);
+    //}
+
+    void Awake ()
     {
         render = GetComponent<SpriteRenderer>();
 	}
@@ -22,7 +101,7 @@ public class Tile : MonoBehaviour {
     private void Select()
     {
         isSelected = true;
-        render.color = selectedColor;
+        gameObject.transform.GetChild(0).GetComponent<Renderer>().enabled = true;
         previousTile = gameObject.GetComponent<Tile>();
         // Play a sound?
     }
@@ -30,7 +109,7 @@ public class Tile : MonoBehaviour {
     private void Deselect()
     {
         isSelected = false;
-        render.color = Color.white;
+        gameObject.transform.GetChild(0).GetComponent<Renderer>().enabled = false;
         previousTile = null;
     }
 
@@ -89,5 +168,44 @@ public class Tile : MonoBehaviour {
             adjacentTiles.Add(GetAdjacent(adjacentCells[i]));
         }
         return adjacentTiles;
+    }
+
+    private string GetRandomShipType()
+    {
+        try
+        {
+            string shipType = ShipTypes[UnityEngine.Random.Range(0, ShipTypes.Count)];
+            return shipType;
+        }
+        catch (Exception ex)
+        {
+            //error handling here
+            Debug.Log("Caught error " + ex.Message);
+            return "none";
+        }
+    }
+
+    private string GetRandomMatchColor ()
+    {
+        try
+        {
+            string matchColor = MatchColors[UnityEngine.Random.Range(0, MatchColors.Count)];
+            return matchColor;
+        }
+        catch (Exception ex)
+        {
+            //error handling here
+            Debug.Log("Caught error " + ex.Message);
+            return "none";
+        }
+    }
+
+    public GameObject GetRight(int count = 1)
+    {
+        //GameObject returnGameObject = BoardManager.boardManager.tiles[];
+
+        GameObject returnGameObject = new GameObject();
+
+        return returnGameObject;
     }
 }
